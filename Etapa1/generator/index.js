fab3f94d704833117bfb846cd76a4602f15b2ea5
alexmodem <a.morenodelmazo@gmail.com>
2020-02-26 15:47:42 +0100

    Generator: First idea

--- Etapa1/generator/index.js
+++ Etapa1/generator/index.js
@@ -1,16 +1,25 @@
-/*AFRAME.registerComponent('option-select', {
-    schema:{ },
+AFRAME.registerComponent('option-select', {
+    schema:{
+        option: {type: 'string'}
+    },
+
+    multiple: true,
 
     init: function () {
-        let data = this.data;
         let el = this.el;
 
         let addEventListener = el.addEventListener('click', (e) => {
 
+            let optDisplay = document.querySelector("#opt");
+            optDisplay.setAttribute('geometry', "primitive: "+e.target.id);
+            optDisplay.setAttribute('material', e.target.getAttribute('material'));
+
+            //Removing the menu
+            el.parentNode.parentNode.removeChild(el.parentNode);
         });
 
     }
-}); */
+});
 
 
 AFRAME.registerComponent('menu-loader', {
@@ -32,12 +41,14 @@ AFRAME.registerComponent('menu-loader', {
             //Including options in the menu
             let polyOpts = data.options;
             for (let option of polyOpts){
-                console.log(polyOpts.indexOf(option)*0.2);
                 let optButton = document.createElement('a-entity');
                 optButton.setAttribute('geometry', "primitive: "+option);
+                optButton.setAttribute('id', option);
+                optButton.setAttribute('option-select',option);
                 optButton.setAttribute('position', ((polyOpts.indexOf(option)*0.3)-0.3)+" 0 0.1");
                 optButton.setAttribute('scale', "0.1 0.3 0.1");
                 optButton.setAttribute('material', "color: red");
+                optButton.setAttribute('animation', "property: rotation; from: 90 90 0 ;to: 450 450 0; loop: true; dur: 3000;");
                 menu.appendChild(optButton);
             }
 
@@ -48,24 +59,35 @@ AFRAME.registerComponent('menu-loader', {
 });
 
 
-
-
-
 AFRAME.registerComponent('polygenerator', {
     schema:{},
 
     init: function () {
-        let data = this.data;
         let el = this.el;
 
         el.addEventListener('click', (e) => {
+            let selection = document.querySelector("#opt");
             let poly = document.createElement('a-entity');
-            poly.setAttribute('geometry', "primitive: cone");
+            poly.setAttribute('id', "polygon");
+            poly.setAttribute('geometry', selection.getAttribute('geometry'));
             poly.setAttribute('position', "1 1.25 -3");
-            poly.setAttribute('scale', "0.5 0.5 0.5");
-            poly.setAttribute('material', "color: blue");
+            poly.setAttribute('material', selection.getAttribute('material'));
             el.sceneEl.appendChild(poly);
         });
 
+    }
+});
+
+AFRAME.registerComponent('color-changer', {
+    schema:{},
+
+    multiple: true,
+
+    init: function () {
+        let el = this.el;
+        let addEventListener = el.addEventListener('click', (e) => {
+            let poly = document.querySelector("#polygon");
+            poly.setAttribute('material', e.target.getAttribute('material'));
+        });
     }
 });
